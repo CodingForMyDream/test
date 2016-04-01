fcb396cd87b781c6b3e525193a8a1a3e6a168133
CodingForMyDream <cuijiajun1989@126.com>
2016-04-01 17:29:01 +0800

    Future cancel()

--- MytestForJava/src/com/qun/thread/CallableFutureTask.java
+++ MytestForJava/src/com/qun/thread/CallableFutureTask.java
@@ -23,16 +23,30 @@ public class CallableFutureTask {
 		FutureTask<Integer> futuret2 = new FutureTask<Integer>(ft2);
 		
 		/**
+		 * 1，线程执行cancel后不能调用get获取结果
+		 * 2，cancel param true立刻打断；false允许执行完成
+		 * 3，线程执行完毕后调用cancel返回false
+		 */
+		executor.submit(futuret);
+		executor.shutdown();
+		System.out.println("线程刚刚启动，线程是否执行完成：" + futuret.isDone());
+		Thread.sleep(100);
+		System.out.println("线程执行以上睡眠时间后，取消线程：" + futuret.cancel(true));
+		System.out.println(futuret.isDone());
+		System.out.println(futuret.cancel(true));
+		
+		
+		/**
 		 * result3的执行结果 == 4950，result1和result2的结果不确定
 		 * 得出结论：result1和result2共享线程的sum
 		 */
-		Future<Integer> result1 = executor.submit(ft);
+		/*Future<Integer> result1 = executor.submit(ft);
 		Future<Integer> result2 = executor.submit(ft);
 		Future<Integer> result3 = executor.submit(ft2);
 		executor.shutdown();
 		System.out.println("线程执行结果1:" + result1.get());
 		System.out.println("线程执行结果2:" + result2.get());
-		System.out.println("线程执行结果3:" + result3.get());
+		System.out.println("线程执行结果3:" + result3.get());*/
 		
 		
 		/**
@@ -80,10 +94,12 @@ class FTask implements Callable<Integer>{
 	
 	@Override
 	public Integer call() throws Exception {
-		//int sum = 0;
 		for(int i = 0; i < 100; i++){
+			Thread.sleep(10);
+			System.out.println("active" + i);
 			sum += i;
 		}
+		
 		return sum;
 	}
 	
